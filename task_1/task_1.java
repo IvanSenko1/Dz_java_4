0503abe7f17985faac629c943fc7ce2f3dacdd61
Ivan <senko.ivan999@mail.ru>
2023-01-28 09:27:37 +0300

    Задача_3

--- task_1/task_1.java
+++ task_1/task_1.java
@@ -6,16 +6,16 @@ import java.nio.charset.*;
 public class task_1 {
 	public static void main(String[] args) {
 		try {
-			FileWriter writingToFile = new FileWriter("Task_1/Task_1.txt", Charset.forName("UTF-8")); // запись в файл
-			writingToFile.append("Работа с файлом: загрузка строки в файл!");
-			writingToFile.flush();
-
-			String line = "";
-			FileReader readingFromFile = new FileReader("Task_1/Task_1.txt", Charset.forName("UTF-8")); // чтение из файла
-			while (readingFromFile.ready()) {
-				line += (char)readingFromFile.read();
+			try (FileWriter writingToFile = new FileWriter("Task_1/Task_1.txt", Charset.forName("UTF-8"))) {
+				writingToFile.append("Работа с файлом: загрузка строки в файл!");
+				writingToFile.flush();
 			}
-
+			String line = "";
+			try (FileReader readingFromFile = new FileReader("Task_1/Task_1.txt", Charset.forName("UTF-8"))) {
+				while (readingFromFile.ready()) {
+					line += (char)readingFromFile.read();
+				}
+			}
 			System.out.println("\n\033[34m" + line + "\033[0m");
 
 		} catch (IOException e) {
